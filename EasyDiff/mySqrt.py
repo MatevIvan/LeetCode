1afee420bf6916f10f358c9203dfd95cf750b8ed
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-08-29 11:05:43 -0700

    mySqrt sorta complete but needs to be redone

---
+++ EasyDiff/mySqrt.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def mySqrt(self, x):
+        i = 0
+        while i <= x:
+            if i * i == x:
+                return i
+            if i * i > x:
+                return i - 1
+            i+=1
+
+
+solutionInst = Solution()
+print(solutionInst.mySqrt(0))
