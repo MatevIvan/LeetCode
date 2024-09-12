6ef084ac81658eb2c57d534828bbe127cc868244
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-09-12 13:30:01 -0700

    Pascal's Triangle 2 - updated

--- EasyDiff/pascalTriangle2.py
+++ EasyDiff/pascalTriangle2.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        # if the number of rows is 1 or 2, hardcode the return value
+        # if the row index is 0 or 1, hardcode the return value
         if rowIndex == 0:
             return [1]
         if rowIndex == 1:
@@ -13,9 +13,9 @@ class Solution(object):
         triangle = [[1],[1,1]]
         # create a temp array
         temp = []
-        # the first loop will go from the 2nd row up to the desired size
+        # the first loop will go from the 2nd row up to the desired row
         for i in range(2,rowIndex+1):
-            # create a temp array at the start of every loop with just the number 1
+            # reset the temp array at the start of every loop with just the number 1
             temp = [1]
             # This second loop will go through the section of the triangle above itself and do the math for the new row
             for j in range(0,len(triangle[i-1])-1):
@@ -24,9 +24,9 @@ class Solution(object):
             temp.append(1)
             # add the temp array to the existing triangle array
             triangle.append(temp)
-        # return the 2d triangle array
+        # return the temp array which is the rowIndex row
         return temp
 
 # test the code 
 solutionInst = Solution()
-print(solutionInst.getRow(4)) # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+print(solutionInst.getRow(4)) # [1, 4, 6, 4, 1]
