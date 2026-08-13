9aaa637518da33a08a3f4f2d126883e4e5662f43
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2026-08-13 13:12:14 -0700

    puzzle 2958

---
+++ MedDiff/length_of_largest_subarray_with_k_frequency.go
@@ -0,0 +1,27 @@
+package meddiff
+
+
+func MaxSubarrayLength(nums []int, k int) int {
+	usedNumbers := make(map[int]int)
+	lowerIndex := 0
+	longestSubarray := 0
+
+	// create expanding right-side window
+	for upperIndex, num := range nums {
+		// inc each new used num
+		usedNumbers[num]++
+
+		// close in the left-side window
+		for usedNumbers[num] > k {
+			usedNumbers[nums[lowerIndex]]--
+			lowerIndex++
+		}
+
+		windowLength := upperIndex - lowerIndex + 1
+		if windowLength > longestSubarray {
+			longestSubarray = windowLength
+		}
+	}
+
+	return longestSubarray
+}
